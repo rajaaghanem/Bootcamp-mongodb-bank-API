6d172232fe8ba41b8006898362f504a63de29691
rajaaghanem <rajaaghanem@gmail.com>
2022-01-20 19:40:28 +0200

    index

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-const express = require("express");
-require("./src/db/mongoose");
-const cors = require('cors');
-const path = require('path');
-
-const productsRoute = require ('./src/routers/product.route.js')
-// const Product = require("./models/product");
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-const publicPath = path.join(__dirname, 'client/build');
-app.use(cors());
-app.use(express.static(publicPath));
-
-app.use(express.json());
-app.use(productsRoute);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(publicPath, '/index.html'));
-});
-
-app.listen(port, () => {
-  console.log("Server is up on port" + port);
-});
-=======
 const express = require("express");
 require("./src/db/mongoose");
 const cors = require('cors');
@@ -50,4 +23,3 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
   console.log("Server is up on port" + port);
 });
->>>>>>> c2507cc368eb311501cbedd5e983019c04d93604
