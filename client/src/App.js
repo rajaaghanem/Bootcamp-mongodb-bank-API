8506793ec9a6e1d6f30f406777b0e3060d01a753
rajaaghanem <rajaaghanem@gmail.com>
2022-01-21 21:04:31 +0200

    add all axios functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,170 @@
-// import './App.css';
+import './App.css';
 import myApi from "./api/Api";
+import React, { useState } from "react";
+
 
 function App() {
+
+  const [userId, setUserId] = useState("");
+  const [passportId, setPassportId] = useState("");
+  const [amount, setAmount] = useState("");
+  const [transfer, setTransfer] = useState("");
+  const [error, setError] = useState("");
+  const [users, setUsers] = useState([]);
+
   //get all users
   const getReq = async () => {
-    const { data } = await myApi.get("/users");
-    console.log(data);
+    setUsers([]);
+    setError("");
+    try{
+      const { data } = await myApi.get("/users");
+      setUsers(data);
+      console.log(data);
+    }catch(e){
+      setError(e.message);
+      console.log(error);
+    }
   };
+
+  const handleId =(e)=>{
+    setUserId(e.target.value);
+  }
+
+  const handlePassport=(e)=>{
+    setPassportId(e.target.value);
+  }
+
+  const handleAmount=(e)=>{
+    setAmount(e.target.value);
+  }
+
+  const  handleTransfer=(e)=>{
+    setTransfer(e.target.value);
+  }
 
   const postReq = async () => {
+    setUsers([]);
+    setError("");
     let obj = {
-      passID: "1213",
-      cash: 1000,
-      credit: 200,
+      passID: passportId,
+      cash: Number(amount) || 0,
+      credit: 0,
     };
-    const { data } = await myApi.post("/users", obj);
-    console.log(data);
+    try{
+      if (!passportId || !amount) {
+        throw new Error("One field or more are missing");
+      }
+      const { data } = await myApi.post("/users", obj);
+      setUsers([data]);
+      console.log(data);
+
+    }catch(e){
+      setError(e.message);
+      console.log(error);
+    }
   };
 
+  // deposit cash to a user
+  const postDepositeReq = async ()=>{
+    setUsers([]);
+    setError("");
+    const obj ={
+      amount: Number(amount)
+    }
+    try{
+      if(!userId) throw new Error("Must Enter an id");
+      const { data } = await myApi.patch(`/users/deposite/${userId}`, obj);
+      setUsers([data]);
+      console.log(data);
+    }catch(e){
+      setError(e.message);
+      console.log(error);
+    }
+  }
+
+  //add credit amount to the user credit
+  const postCreditReq = async ()=>{
+    setUsers([]);
+    setError("");
+    const obj ={
+      amount: Number(amount)
+    }
+    try{
+      if(!amount) throw new Error("Amount of money is missing");
+      const { data } = await myApi.patch(`/users/credit/${userId}`, obj);
+      setUsers([data]);
+      console.log(data);
+    }catch(e){
+      setError(e.message);
+      console.log(error);
+    }
+  }
+
+  const postWithdrawReq = async ()=>{
+    setUsers([]);
+    setError("");
+    const obj ={
+      amount: Number(amount)
+    }
+    try{
+      if(!amount) throw new Error("Amount of money is missing");
+      const { data } = await myApi.patch(`/users/withdraw/${userId}`, obj);
+      setUsers([data]);
+      console.log(data);
+    }catch(e){
+      setError(e.message);
+      console.log(error);
+    }
+  }
+
+
+   const postTransferReq = async ()=>{
+    setUsers([]);
+    setError("");
+     const obj = {
+       amount:  Number(amount),
+       id: transfer,
+     }
+    try{
+      if(!amount || !transfer) throw new Error("One field or more are missing")
+      const { data } = await myApi.patch(`/users/transfer/${userId}`, obj);
+      setUsers(data);
+      console.log(data);
+
+    }catch(e){
+      setError(e.message);
+      console.log(error);
+    }
+  }
+
+  const showUsers=()=>{
+    return users.map ((user)=>{
+      return (<div  className = "card-container" key={user.passID}>{`passport id: ${user.passID} cash: ${user.cash} credit: ${user.credit}`}</div>)
+    })
+  }
+
   return (
-    <div className="App">
-      {" "}
-      Hello World!
-      <button onClick={getReq}>get</button>
-      <button onClick={postReq}>add user</button>
+    <div className="app-container">
+      <div className="inputs-container">
+     <div><label>User id: </label><input onChange={handleId} value={userId}></input></div> 
+      <div><label>user passport id: </label><input onChange={handlePassport} value={passportId}></input></div>
+      <div><label>amount of money: </label><input  onChange={handleAmount} value={amount}></input></div>
+      <div><label>client to transfer: </label><input onChange={handleTransfer} value={transfer}></input></div>
+      </div>
+      
+      <div className="buttons-container">
+      <div><button onClick={getReq}>get all users</button></div>
+      <div><button >get user</button></div>
+      <div><button onClick={postReq}>add user</button></div>
+      <div><button onClick={postDepositeReq}>deposite cash</button></div>
+      <div><button onClick={postCreditReq}>update credit</button></div>
+      <div><button onClick={postWithdrawReq}>withdraw money</button></div>
+      <div><button onClick={postTransferReq}>transfer money</button></div>
+      </div>
+      
+      <div className='cards-container'>{showUsers()}</div>
+      {error}
+      
     </div>
   );
 }
--- src/routers/user.route.js
+++ src/routers/user.route.js
@@ -22,44 +22,6 @@ router.get("/api/users", async (req, res) => {
   }
 });
 
-//Get products that are active
-// router.get("/api/users/active", async (req, res) => {
-//   try {
-//     const products = await Product.find({ isActive: true });
-//     res.status(200).send(products);
-//   } catch (e) {
-//     res.status(400).send(e);
-//   }
-// });
-
-//Get products with a specific price range
-// router.get("/api/users/range", async (req, res) => {
-//   try {
-//     const products = await Product.find({
-//       $and: [
-//         { "details.Price": { $lt: req.query.max } },
-//         { "details.Price": { $gt: req.query.min } },
-//       ],
-//     });
-//     res.status(200).send(products);
-//   } catch (e) {
-//     res.status(400).send(e);
-//   }
-// });
-
-//Get a specific product by id
-// router.get("/api/users/:id", handleResponse);
-
-// async function handleResponse(req, res) {
-//   const _id = req.params.id;
-//   try {
-//     const product = await Product.findById(_id);
-//     if (!product) return res.status(404).send();
-//     res.status(200).send(product);
-//   } catch (e) {
-//     res.status(400).send(e.message);
-//   }
-// }
 
 //Can deposit cash to a user
 router.patch("/api/users/deposite/:id", handleDeposite);
@@ -67,6 +29,8 @@ router.patch("/api/users/deposite/:id", handleDeposite);
 async function handleDeposite(req, res) {
   const _id = req.params.id;
   const amount = req.body.amount;
+
+  console.log("amount", amount);
 
   const updates = Object.keys(req.body);
   const allowedUpdates = ["amount"];
@@ -80,7 +44,7 @@ async function handleDeposite(req, res) {
 
   try {
     const user = await User.findById(_id);
-    if (!user) return res.status(404).send();
+    if (!user) return res.status(404).send("user not found");
     const editedUser = depositingUser(user, amount);
     const updatedUser = await User.findByIdAndUpdate(_id, editedUser, {
         new: true,
@@ -117,7 +81,7 @@ async function handleCredit(req, res){
 
     try{
         const user = await User.findById(_id);
-        if (!user) return res.status(404).send();
+        if (!user) return res.status(404).send("user not found");
         const editedUser = creditUser(user, amount);
         const updatedUser = await User.findByIdAndUpdate(_id, editedUser, {
         new: true,
@@ -226,44 +190,5 @@ function enoughMoney(theUser, moneyAmount) {
     }
   }
 
-//update a product to become active/not active and change the value of its discount.
-// router.patch("/api/users/active/:id", async (req, res) => {
-//   const _id = req.params.id;
-
-//   const updates = Object.keys(req.body);
-//   const allowedUpdates = ["isActive", "details.discount"];
-//   const isValidOperation = updates.every((update) =>
-//     allowedUpdates.includes(update)
-//   );
-
-//   if (!isValidOperation) {
-//     return res.status(400).send({ error: "invalid updates" });
-//   }
-
-//   try {
-//     const editedProduct = await Product.findByIdAndUpdate(_id, req.body, {
-//       new: true,
-//       runValidators: true,
-//     });
-
-//     if (!editedProduct) return res.status(404).send();
-//     res.status(200).send(editedProduct);
-//   } catch (e) {
-//     res.status(400).send(e.message);
-//   }
-// });
-
-// Delete a specific product
-// router.delete("/api/users/:id", async (req, res) => {
-//   const _id = req.params.id;
-
-//   try {
-//     const product = await Product.findByIdAndDelete(_id);
-//     if (!product) return res.status(404).send();
-//     res.status(200).send(product);
-//   } catch (e) {
-//     res.status(400).send(e.message);
-//   }
-// });
 
 module.exports = router;
