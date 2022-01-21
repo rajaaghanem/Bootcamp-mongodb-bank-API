34e7a7aef5936ce95a6de456f091c9d84e091526
rajaaghanem <rajaaghanem@gmail.com>
2022-01-21 17:52:16 +0200

    get users

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,27 @@ import myApi from "./api/Api";
 
 function App() {
   const getReq = async () => {
-    const { data } = await myApi.get("/products");
+    const { data } = await myApi.get("/users");
     console.log(data);
   };
 
-  const postReq = async () => {
-    let obj = {
-      name: "red hat",
-      category: "hats",
-      isActive: true,
-        details: {
-          description: "nice stylish yellow hat ",
-          Price: 30,
-          discount: 0,
-          array: ["hat", "yellow"],
-          phone: "0526879443"
-        }
-      }
+  // const postReq = async () => {
+  //   let obj = {
+  //     name: "red hat",
+  //     category: "hats",
+  //     isActive: true,
+  //       details: {
+  //         description: "nice stylish yellow hat ",
+  //         Price: 30,
+  //         discount: 0,
+  //         array: ["hat", "yellow"],
+  //         phone: "0526879443"
+  //       }
+  //     }
     // obj= JSON.stringify(obj)
-    const { data } = await myApi.post("/products", obj);
-    console.log(data);
-  };
+  //   const { data } = await myApi.post("/products", obj);
+  //   console.log(data);
+  // };
 
   return (
     <div className="App">
