8fb0df61457762f77f38e35e65d2ff6bbd9e3158
rajaaghanem <rajaaghanem@gmail.com>
2022-01-21 17:44:19 +0200

    string url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URL, (error, client) => {
+mongoose.connect(`${process.env.MONGO_URL}`, (error, client) => {
   if (error) {
     return console.log("unable to connect to database");
   }
